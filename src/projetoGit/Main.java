d9836c54278c53e489d86a21d9a63dd76f3d4ee1
Rafael Roque Viana <rafa834@gmail.com>
2021-03-01 19:36:53 -0300

    novo commit master

--- src/projetoGit/Main.java
+++ src/projetoGit/Main.java
@@ -3,10 +3,7 @@ package projetoGit;
 public class Main {
 	
 	public static void main(String[] args) {
-
-		System.out.println("Merge realizado");
-System.out.println("Linha adicionada alterada pela master e alterada pela branch");
-		
+String x = "1";		
 
 	}
 
